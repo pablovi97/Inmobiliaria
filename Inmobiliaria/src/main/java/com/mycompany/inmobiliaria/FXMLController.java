065583213d72198056c0abe01d5bf3ce48d96d86
Pablo Viera <32093629+pablovi97@users.noreply.github.com>
2019-05-10 17:28:01 +0100

    cambio 1
    sdf

---
+++ Inmobiliaria/src/main/java/com/mycompany/inmobiliaria/FXMLController.java
@@ -0,0 +1,86 @@
+package com.mycompany.inmobiliaria;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private TextField txIdUser;
+    @FXML
+    private TextField txNombreUser;
+    @FXML
+    private TextField txApellidosUser;
+    @FXML
+    private Button btnAtrasUser;
+    @FXML
+    private Button btnCrearUser;
+    @FXML
+    private Button btnAdelanteUser;
+    @FXML
+    private Button btnEliminarUser;
+    @FXML
+    private Button btnModificarUser;
+    @FXML
+    private Button btnOkUser;
+    @FXML
+    private Button btnCancelarUser;
+    @FXML
+    private Button btnAtrasCasa;
+    @FXML
+    private Button btnAgregarCasa;
+    @FXML
+    private Button btnAdelanteCasa;
+    @FXML
+    private Button btnQuitarCasa;
+    @FXML
+    private TextField txIdCasa;
+    @FXML
+    private TextField txMetrosCasa;
+    @FXML
+    private TextField txHabitacionesCasa;
+    @FXML
+    private TextField txDomicilioCasa;
+    @FXML
+    private CheckBox chkGarajeCasa;
+    @FXML
+    private Button btnOkCasa;
+    @FXML
+    private Button btnCancelarCasa;
+    @FXML
+    private Button btnAtrasCasa_Casas;
+    @FXML
+    private Button btnAgregarCasa_Casas;
+    @FXML
+    private Button btnAdelanteCasa_Casas;
+    @FXML
+    private Button btnQuitarCasa_Casas;
+    @FXML
+    private TextField btnIdCasa_Casas;
+    @FXML
+    private TextField txMetrosCasa_Casas;
+    @FXML
+    private TextField txHabitacionesCasa_Casas;
+    @FXML
+    private TextField txDomicilioCasa_Casas;
+    @FXML
+    private CheckBox chkGarajeCasa_Casas;
+    @FXML
+    private Button btnOkCasa_Casas;
+    @FXML
+    private Button btnCancelarCasa_Casas;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+    
+}
